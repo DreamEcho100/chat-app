d6f6f7ad0c3505ad0a2caf416daf4f89c5f16d77
DreamEcho100 <maze6572198@gmail.com>
2024-03-24 11:04:33 +0200

    working on the `Search` functionality

--- frontend/src/pages/home/_components/sidebar/search-input.jsx
+++ frontend/src/pages/home/_components/sidebar/search-input.jsx
@@ -1,32 +1,62 @@
-import { IoSearchSharp } from 'react-icons/io5';
+import { useMemo } from "react";
+import { IoSearchSharp } from "react-icons/io5";
+import useUserConversations from "../../../../libs/utils/zustand/user-conversations";
+import toast from "react-hot-toast";
+import useQueryManager from "../../../../libs/utils/hooks/query-manager";
+import { z } from "zod";
 
-const SearchInput = () => {
+const sendMessageSchema = z.object({
+	search: z.string().min(3),
+});
+
+export default function SearchInput() {
+	const { setSelectedConversation } = useUserConversations();
+
+	const { query } = useQueryManager({
+		queryKey: "conversations",
+		// input: props.input,
+		queryFn: async () => {
+			return await fetch("/api/users");
+		},
+		/** @param {{ data: import("../../../../libs/utils/types").Conversation[] }} _data */
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		onSuccess: (_data) => {
+			console.log("Success getting conversations");
+		},
+		stopDefault: { successHandling: true },
+	});
+
+	const conversations = useMemo(() => query.data?.data ?? [], [query.data?.data]);
+
 	return (
-		<form className="flex items-center gap-2">
-			<input
-				type="text"
-				placeholder="Search…"
-				className="input input-bordered rounded-full"
-			/>
-			<button type="submit" className="btn btn-circle bg-sky-500 text-white">
+		<form
+			onSubmit={(event) => {
+				event.preventDefault();
+
+				const formData = new FormData(/** @type {HTMLFormElement} */ (event.target));
+				const search = formData.get("search")?.toString() ?? "";
+
+				const result = sendMessageSchema.safeParse({ search });
+
+				if (!result.success) {
+					return toast.error("Search term must be at least 3 characters long");
+				}
+
+				const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
+
+				if (conversation) {
+					setSelectedConversation(conversation);
+					/** @type {HTMLFormElement} */ (event.target).reset();
+				} else {
+					toast.error("No such user found!");
+				}
+			}}
+			className="flex items-center gap-2"
+		>
+			<input name="search" placeholder="Search…" className="rounded-full input input-bordered" />
+			<button type="submit" className="text-white btn btn-circle bg-sky-500">
 				<IoSearchSharp className="w-6 h-6 outline-none" />
 			</button>
 		</form>
 	);
-};
-export default SearchInput;
-
-// STARTER CODE SNIPPET
-// import { IoSearchSharp } from "react-icons/io5";
-
-// const SearchInput = () => {
-// 	return (
-// 		<form className='flex items-center gap-2'>
-// 			<input type='text' placeholder='Search…' className='input input-bordered rounded-full' />
-// 			<button type='submit' className='btn btn-circle bg-sky-500 text-white'>
-// 				<IoSearchSharp className='w-6 h-6 outline-none' />
-// 			</button>
-// 		</form>
-// 	);
-// };
-// export default SearchInput;
+}
